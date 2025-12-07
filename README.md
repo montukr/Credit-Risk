b13668ad89a5a279f6bf0f642d64a8f4ff3bdbbc
montukr <krxmontu@gmail.com>
2025-12-07 11:35:34 +0530

    Stop tracking files listed in .gitignore

--- README.md
+++
@@ -1,8 +0,0 @@
-Run Commands- 
-backend- 
-cd backend 
-conda activate credit-risk 
-uvicorn app.main:app --reload 
-frontend- 
-cd frontend
-npm run dev
--- backend/app/schemas/transaction.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional
-
-class TransactionCreate(BaseModel):
-    amount: float = Field(gt=0)
-    category: str
-    description: Optional[str] = None
